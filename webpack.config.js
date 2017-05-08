efdbe514d1f1c9a846364d685b051cab221d618a
James Dalessio <jrd261@gmail.com>
2017-05-08 17:19:17 -0400

    Remove cruft.

--- webpack.config.js
+++ webpack.config.js
@@ -77,10 +77,6 @@ module.exports = env => {
       // Automatically generates HTML file
       new HTMLPlugin({
         template: './index.ejs',
-
-        options: {
-          appMountId: 'main'
-        }
       })
 
     ]
