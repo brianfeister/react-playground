9ecccfb282681301d5a6d28bb26b9c1c3ee88efe
James Dalessio <jrd261@gmail.com>
2017-05-10 16:56:10 -0400

    Remove babel field.

--- core/utils/combineReducers.js
+++ core/utils/combineReducers.js
@@ -1,6 +1,6 @@
 'use strict';
 
-export default (namespace, reducers) => (state={}, action) => {
+/*export default (namespace, reducers) => (state={}, action) => {
   if (!action.type.startsWith(namespace)) return state;
   //return combineReducers(reducers);
-};
+};*/
